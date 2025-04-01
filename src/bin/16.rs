AdrianColaianni/aoc-rust-2024

commit 1f308bc524bdd7392f512929c93802195975b78e

Cleanup and readme

src/bin/16.rs
@@ -1,10 +1,8 @@
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::{BinaryHeap, HashMap};
 
 advent_of_code::solution!(16);
 
 type Map = Vec<Vec<char>>;
-
-// type Prev = Vec<Vec<Vec<(usize, usize)>>>;
 
 #[derive(PartialEq, Eq, Clone, Copy, Hash)]
 struct Pos {
@@ -13,14 +11,6 @@ struct Pos {
     o: usize, // Orientation: 0: North, 1: East, 2: South, 3: West
     cost: usize,
 }
-
-// impl std::hash::Hash for Pos {
-//     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-//         self.r.hash(state);
-//         self.c.hash(state);
-//         self.o.hash(state);
-//     }
-// }
 
 impl std::fmt::Debug for Pos {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -112,6 +102,16 @@ pub fn part_one(input: &str) -> Option<usize> {
     None
 }
 
+fn count_spaces(p: &Pos, prev: &HashMap<Pos, Vec<Pos>>) -> usize {
+    if let Some(p) = prev.get(p) {
+        p.into_iter()
+            .map(|p| count_spaces(p, prev))
+            .sum()
+    } else {
+        1
+    }
+}
+
 pub fn part_two(input: &str) -> Option<usize> {
     let map: Map = input.lines().map(|l| l.chars().collect()).collect();
     let size = map.len();
@@ -126,11 +126,11 @@ pub fn part_two(input: &str) -> Option<usize> {
     let mut heap: BinaryHeap<Pos> = BinaryHeap::new();
     heap.push(Pos::new(size - 2, 1, 0));
 
-    let mut trace = vec![];
+    let mut end = Pos::new(1, size - 2, 0);
 
     while let Some(pos) = heap.pop() {
         if pos.r == 1 && pos.c == size - 2 {
-            trace.push(pos);
+            end = pos;
             break;
         }
 
@@ -151,19 +151,7 @@ pub fn part_two(input: &str) -> Option<usize> {
         }
     }
 
-    let mut spath = HashSet::new();
-    while !trace.is_empty() {
-        for p in &trace {
-            spath.insert((p.r, p.c));
-        }
-        trace = trace
-            .into_iter()
-            .filter_map(|p| prev.get(&p).map(|v| v.clone()))
-            .flatten()
-            .collect();
-    }
-
-    Some(spath.len())
+    Some(count_spaces(&end, &prev))
 }
 
 #[cfg(test)]

src/bin/12.rs
@@ -163,26 +163,6 @@ impl Dir {
         }
         Some(pos)
     }
-
-    fn move_right(&self, mut pos: Pos, bounds: &Pos) -> Option<Pos> {
-        match self {
-            Dir::Up => {
-                pos.col += 1;
-                if pos.col == bounds.col {
-                    return None;
-                }
-            }
-            Dir::Right => {
-                pos.row += 1;
-                if pos.row == bounds.row {
-                    return None;
-                }
-            }
-            Dir::Down => pos.col = pos.col.checked_sub(1)?,
-            Dir::Left => pos.row = pos.row.checked_sub(1)?,
-        }
-        Some(pos)
-    }
 }
 
 fn find_plot(input: &Vec<Vec<char>>, pos: Pos, bounds: &Pos) -> Vec<Pos> {
@@ -206,7 +186,6 @@ fn find_plot(input: &Vec<Vec<char>>, pos: Pos, bounds: &Pos) -> Vec<Pos> {
 }
 
 struct Region {
-    area: Vec<Pos>,
     size: usize,
     sides: usize,
 }
@@ -215,7 +194,6 @@ impl Region {
     fn new(area: Vec<Pos>, sides: usize) -> Self {
         Self {
             size: area.len(),
-            area,
             sides,
         }
     }
@@ -253,7 +231,6 @@ pub fn part_one(input: &str) -> Option<u32> {
             for pos in &area {
                 ignore.insert(*pos);
             }
-            // println!("{}: {}, {}", input[row][col], area.len(), bounds(&area));
             total += area.len() * bounds(&area);
         }
     }
@@ -274,8 +251,6 @@ fn on_boarder(input: &Vec<Vec<char>>, pos: &Pos, dir: Dir, limits: &Pos) -> bool
 // We make a little guy who walks around and counts the sides
 // Return sides and corners on sides
 fn bounds_p2(input: &Vec<Vec<char>>, area: &Vec<Pos>, limits: &Pos) -> usize {
-    let pos = area.first().unwrap();
-    println!("Checking bounds for {}: {:?}", input[pos.row][pos.col], area);
     if area.len() == 1 {
         // My program cannot handle size 1, so we hard code :)
         return 4;
@@ -293,7 +268,6 @@ fn bounds_p2(input: &Vec<Vec<char>>, area: &Vec<Pos>, limits: &Pos) -> usize {
             if visited.contains(&(pos, dir)) || !on_boarder(input, &pos, dir, limits) {
                 continue;
             }
-            println!("Little guy is running: {:?} {:?}", pos, dir);
             visited.insert((pos, dir));
             loop {
                 if them_turns != 0 && *start == pos && start_dir == dir {
@@ -303,7 +277,6 @@ fn bounds_p2(input: &Vec<Vec<char>>, area: &Vec<Pos>, limits: &Pos) -> usize {
                 //     panic!("Exceeded turn limit");
                 // }
 
-                println!("Little guy: {:?} {:?}", pos, dir);
 
                 if let Some(next_pos) = dir.wrap_left(pos, limits) {
                     let mut ndir = dir;
@@ -312,12 +285,10 @@ fn bounds_p2(input: &Vec<Vec<char>>, area: &Vec<Pos>, limits: &Pos) -> usize {
                     let border = border.is_none()
                         || border.is_some_and(|p| input[p.row][p.col] != input[pos.row][pos.col]);
                     if area.contains(&next_pos) && border {
-                        println!("Wraping left");
                         dir.turn_left();
                         them_turns += 1;
                         pos = next_pos;
                         if !visited.insert((pos, dir)) && *start != pos && start_dir != dir {
-                            println!("We've been here before");
                             them_turns = 0;
                             break;
                         }
@@ -329,10 +300,8 @@ fn bounds_p2(input: &Vec<Vec<char>>, area: &Vec<Pos>, limits: &Pos) -> usize {
                     let border = border.is_none()
                         || border.is_some_and(|p| input[p.row][p.col] != input[pos.row][pos.col]);
                     if area.contains(&next_pos) && border {
-                        println!("Moving forward");
                         pos = next_pos;
                         if !visited.insert((pos, dir)) && *start != pos && start_dir != dir {
-                            println!("We've been here before");
                             them_turns = 0;
                             break;
                         }
@@ -340,11 +309,9 @@ fn bounds_p2(input: &Vec<Vec<char>>, area: &Vec<Pos>, limits: &Pos) -> usize {
                     }
                 }
                 // At dead end, turn around
-                println!("Turning right");
                 dir.turn_right();
                 them_turns += 1;
             }
-            println!("Profited {} turns from that!", them_turns);
             turns += them_turns;
         }
     }
@@ -370,7 +337,6 @@ pub fn part_two(input: &str) -> Option<u32> {
                 ignore.insert(*pos);
             }
             let sides = bounds_p2(&input, &area, &limits);
-            println!("{}: {}, {}", input[row][col], area.len(), sides);
             let region = Region::new(area, sides);
             regions.push(region);
         }

commit 8ed894fe6883528fc35c10b665b89e8f5e8ea223

Fixed day 16 (broke since 1f308bc)

src/bin/16.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 advent_of_code::solution!(16);
 
@@ -102,16 +102,6 @@ pub fn part_one(input: &str) -> Option<usize> {
     None
 }
 
-fn count_spaces(p: &Pos, prev: &HashMap<Pos, Vec<Pos>>) -> usize {
-    if let Some(p) = prev.get(p) {
-        p.into_iter()
-            .map(|p| count_spaces(p, prev))
-            .sum()
-    } else {
-        1
-    }
-}
-
 pub fn part_two(input: &str) -> Option<usize> {
     let map: Map = input.lines().map(|l| l.chars().collect()).collect();
     let size = map.len();
@@ -126,11 +116,11 @@ pub fn part_two(input: &str) -> Option<usize> {
     let mut heap: BinaryHeap<Pos> = BinaryHeap::new();
     heap.push(Pos::new(size - 2, 1, 0));
 
-    let mut end = Pos::new(1, size - 2, 0);
+    let mut trace = vec![];
 
     while let Some(pos) = heap.pop() {
         if pos.r == 1 && pos.c == size - 2 {
-            end = pos;
+            trace.push(pos);
             break;
         }
 
@@ -151,7 +141,19 @@ pub fn part_two(input: &str) -> Option<usize> {
         }
     }
 
-    Some(count_spaces(&end, &prev))
+    let mut path = HashSet::new();
+    while !trace.is_empty() {
+        for p in &trace {
+            path.insert((p.r, p.c));
+        }
+        trace = trace
+            .into_iter()
+            .filter_map(|p| prev.get(&p).map(|v| v.clone()))
+            .flatten()
+            .collect();
+    }
+
+    Some(path.len())
 }
 
 #[cfg(test)]